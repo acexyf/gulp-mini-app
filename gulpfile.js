0bcfe258d3b24f52698e7325d8867cce80408f50
corner <corner_scofield@163.com>
2019-05-20 11:37:21 +0800

    添加glob

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var less = require('gulp-less');
-var glob = require("glob")
+var glob = require("glob");
+var rename = require('gulp-rename');
 
 //环境变量
 var env = process.env.NODE_ENV || 'prod';
@@ -11,7 +12,7 @@ var buildFile = '../build';
 
 
 gulp.task('common-file',function(){
-    return gulp.src(['./app.js','./app.json','./project.config.json', './sitemap.json', './app.wxss'])
+    return gulp.src(glob.sync('./@(app.js|app.json|sitemap.json|project.config.json|app.wxss)'))
             .pipe(gulp.dest(`${buildFile}`))
 })
 
@@ -19,7 +20,7 @@ gulp.task('common-file',function(){
 
 
 gulp.task('watch',function(){
-    gulp.watch(['./app.js','./app.json','./project.config.json', './sitemap.json', './app.wxss'], gulp.series('common-file'))
+    gulp.watch(glob.sync('./@(app.js|app.json|sitemap.json|project.config.json|app.wxss)'), gulp.series('common-file'))
 })
 
 
@@ -30,8 +31,6 @@ if(isProd){
         done()
     }));
 } else {
-
-
 
     gulp.task('default', gulp.series('common-file','watch', function(done){
         console.log('dev');
